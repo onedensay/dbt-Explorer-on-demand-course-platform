2d067bf5554dda36457fac66cfb0b9dfcf35b0ac
Denis Mikhailov <onedensayil@gmail.com>
2025-03-10 20:37:57 +0000

    commit 1

---
+++ models/dim_customers.sql
@@ -0,0 +1,26 @@
+{{
+
+    config(
+        materialized = 'table'
+    )
+
+}}
+
+-- with customers as (
+
+--     select * 
+--     from {{ ref('stg_jaffle_shop__customers') }} 
+
+-- ),
+
+-- orders as (
+
+    select * 
+    from {{ ref('stg_jaffle_shop__orders') }}
+
+-- )
+
+-- customer_orders as (
+--     select 
+
+-- )
